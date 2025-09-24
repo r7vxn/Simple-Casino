cd53fa3f3ed6f3e9ae530e4b77284b82c4fa5baf
Moses Ma <701000372@GoTVDSB.ca>
2025-09-24 09:39:35 -0400

    work on it

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ string project;
 
 bool autimunism = false;
 
+double money = 100;
+
+bool flip;
+
+bool errorloop = true;
+
+Random generator;
+generator = new Random();
+
 while (!done)
 {
     Console.WriteLine(@"
@@ -17,10 +26,48 @@ while (!done)
 
     if (project == "1")
     {
+        while (errorloop)
+        {
+            Console.WriteLine($"You currently own {money.ToString("c")} do you wanna flip or quit. (Enter \"Flip\" / \"Quit\")");
+
+            if (Console.ReadLine().ToLower() == "flip")
+            {
+                flip = true;
+                errorloop = false;
+            }
+            else if (Console.ReadLine().ToLower() == "quit")
+            {
+                flip = false;
+                errorloop = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, try again.");
+            }
+        }
+
+       
+        {
+            if (autimunism == true)
+            {
+                Console.WriteLine("Thank you for playing you autistic fuck");
+            }
+            else
+            {
+                Console.WriteLine("Was a pleasure to have you play here mate, see you sometime again soon!");
+            }
+            done = true;
+        }
+        else
+        {
 
 
 
 
+
+
+
+        }
     }
     if (project == "2")
     {
