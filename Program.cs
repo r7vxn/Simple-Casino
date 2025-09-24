22dc740ac9e6b07bf8211d75da2f434a7486482f
Moses Ma <701000372@GoTVDSB.ca>
2025-09-24 09:08:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿bool done;
+
+done = false;
+
+string project;
+
+bool autimunism = false;
+
+while (!done)
+{
+    Console.WriteLine(@"
+    1. Continue Game
+    2. Quit
+    ");
+
+    project = Console.ReadLine();
+
+    if (project == "1")
+    {
+
+
+
+
+    }
+    if (project == "2")
+    {
+        if (autimunism == true)
+        {
+            Console.WriteLine("Thank you for playing you autistic fuck");
+        }
+        else
+        {
+            Console.WriteLine("Was a pleasure to have you play here mate, see you sometime again soon!");
+        }
+            done = true;
+    }
+    else
+    {
+        autimunism = true;
+        Console.Clear();
+        Console.WriteLine("Invalid input you autistic fuck.");
+    }
+}
